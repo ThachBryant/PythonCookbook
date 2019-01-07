027da58b731bae5d38ba53f570e9296631f7132d
Bryant Thach <bryanthach@gmail.com>
2019-01-06 22:22:18 -0800

    First example

---
+++ Unpack_a_sequence.py
@@ -0,0 +1,9 @@
+# You have N Element tuple that you would like to unpack into a collection of N variables 
+p = (4,5)
+x,y = p
+
+print x
+
+data = ['ACME',50,91.1,(2012,12,21)]
+names,shares,price,date = data
+print names
